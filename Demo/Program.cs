9fed48221b974147ffb90d348fe4ffd829c11089
chenrensong <chenrensong@outlook.com>
2019-05-12 23:09:18 +0800

    添加机动部队m3u8下载示例

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -27,8 +27,16 @@ namespace Demo
             //        Console.WriteLine(stopwatch.ElapsedMilliseconds);
             //    }
             //});
-
-            var m3u8downloader = new M3U8Downloader("https://iqiyi.qq-zuidazy.com/20190506/10108_b2bd8bb2/800k/hls/index.m3u8", "C:\\Test");
+            //机动部队
+            //https://iqiyi.qq-zuidazy.com/20190506/10110_005140a1/800k/hls/index.m3u8
+            //https://iqiyi.qq-zuidazy.com/20190506/10109_da4bd19a/800k/hls/index.m3u8
+            //https://iqiyi.qq-zuidazy.com/20190506/10108_b2bd8bb2/800k/hls/index.m3u8
+            //https://iqiyi.qq-zuidazy.com/20190506/10107_8b4c62e3/800k/hls/index.m3u8
+            //https://iqiyi.qq-zuidazy.com/20190506/10106_32bdc1be/800k/hls/index.m3u8
+            //https://iqiyi.qq-zuidazy.com/20190506/10105_fbb1785b/800k/hls/index.m3u8
+            //https://iqiyi.qq-zuidazy.com/20190506/10104_771a135b/800k/hls/index.m3u8
+            //https://iqiyi.qq-zuidazy.com/20190506/10103_5703cff8/800k/hls/index.m3u8
+            var m3u8downloader = new M3U8Downloader("https://iqiyi.qq-zuidazy.com/20190506/10107_8b4c62e3/800k/hls/index.m3u8", "C:\\Test");
             m3u8downloader.SuggestedFileName = "3.m3u8";
             await m3u8downloader.LoadAsync();
             await m3u8downloader.DownloadFileAsync((e) =>
